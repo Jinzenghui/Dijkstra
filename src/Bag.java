f3232077fe7601053d69b8a184c9f1f0fed56ada
Zenghui.K <1522425758@qq.com>
2015-11-02 22:29:13 +0800

    双栈解析带有括号的算术表达式

---
+++ src/Bag.java
@@ -0,0 +1,73 @@
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
+public class Bag<Item> implements Iterable<Item> {
+	
+	private Node<Item> first;
+	private int N;
+	
+	private static class Node<Item>{
+		private Item item;
+		private Node<Item> next;
+	}
+	
+	public Bag(){
+		first = null;
+		N = 0;
+	}
+	
+	public boolean isEmpty(){
+		return first == null;
+	}
+	
+	public int size(){
+		return N;
+	}
+	
+	public void add(Item item){
+		Node<Item> oldfirst = first;
+		first = new Node<Item>();
+		first.item = item;
+		first.next = oldfirst;
+		N++;
+	}
+
+	@Override
+	public Iterator<Item> iterator() {
+		// TODO Auto-generated method stub
+		return new ListIterator<Item>(first);
+	}
+	
+	private class ListIterator<Item> implements Iterator<Item>{
+		private Node<Item> current;
+		
+		public ListIterator(Node<Item> first){
+			current = first;
+		}
+
+		@Override
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return current != null;
+		}
+		
+		public void remove(){
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public Item next() {
+			// TODO Auto-generated method stub
+			if(!hasNext()) throw new NoSuchElementException();
+			Item item = current.item;
+			current = current.next;
+			return item;
+		}
+		
+		
+	}
+	
+
+}
